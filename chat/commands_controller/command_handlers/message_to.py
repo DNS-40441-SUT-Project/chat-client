b6b284290fead739cabc64102f9799d55e54f2f8
amirsadraabdollahi <abdollahiamirsadra@gmail.com>
2023-06-30 01:52:53 +0330

    add symmetric key

--- chat/commands_controller/command_handlers/message_to.py
+++ chat/commands_controller/command_handlers/message_to.py
@@ -14,7 +14,7 @@ from chat.utils.hash import sha1
 def create_session_with_user(luser: LoggedInUser, other_user: str):
     # 1
     KA = 'KA'
-    connection.send_encrypted(
+    connection.send_sym_encrypted(
         path='start_session', data=dict(
             to=other_user,
             T=datetime.now().timestamp(),
@@ -25,10 +25,10 @@ def create_session_with_user(luser: LoggedInUser, other_user: str):
                 username=luser.username,
                 password=luser.password
             )
-        ), public_key=settings.SERVER_PUB
+        ), symmetric_key=LoggedInUser.get_logged_in_user().encode_symmetric_key
     )
     # 8
-    message = connection.recieve_decrypted(settings.PRIVATE_KEY)
+    message = connection.recieve_sym_decrypted(LoggedInUser.get_logged_in_user().encode_symmetric_key)
     data = message.body
     if data['from'] != other_user:
         raise SecurityException()
@@ -43,7 +43,7 @@ def create_session_with_user(luser: LoggedInUser, other_user: str):
     encrypted_hash_m = DH_encrypt(hash_m, KA, KB)
     M_Prim = 'M_PRIM'
     encrypted_m_prim = DH_encrypt(M_Prim, KA, KB)
-    connection.send_encrypted(
+    connection.send_sym_encrypted(
         path='resume_session', data=dict(
             to=other_user,
             encrypted_hash_m=encrypted_hash_m,
@@ -55,7 +55,7 @@ def create_session_with_user(luser: LoggedInUser, other_user: str):
                 username=luser.username,
                 password=luser.password
             )
-        ), public_key=settings.SERVER_PUB
+        ), symmetric_key=LoggedInUser.get_logged_in_user().encode_symmetric_key
     )
 
 
