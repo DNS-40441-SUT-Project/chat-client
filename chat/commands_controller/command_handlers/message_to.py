db987b0e2ef107e821901862a08e9c6dc181ee9e
amirsadra.abdollahi <amirsadra.abdollahi@cafebazaar.ir>
2023-06-30 01:03:36 +0330

    resume session

--- chat/commands_controller/command_handlers/message_to.py
+++ chat/commands_controller/command_handlers/message_to.py
@@ -1,21 +1,24 @@
+import hashlib
 from datetime import datetime
 
-import rsa
 from django.conf import settings
 
 from chat.data import LoggedInUser
 from chat.exceptions import SecurityException
 from chat.models import UserSecret
 from chat.utils import connection
+from chat.utils.defi_helman import DH_decrypt, DH_encrypt
+from chat.utils.hash import sha1
 
 
 def create_session_with_user(luser: LoggedInUser, other_user: str):
     # 1
+    KA = 'KA'
     connection.send_encrypted(
         path='start_session', data=dict(
             to=other_user,
             T=datetime.now().timestamp(),
-            KA='KA',
+            KA=KA,
         ),
         headers=dict(
             authentication=dict(
@@ -32,10 +35,28 @@ def create_session_with_user(luser: LoggedInUser, other_user: str):
     if data['T'] - datetime.now().timestamp() > 10:
         raise SecurityException()
     KB = data['KB']
-    print('Bye')
+    encrypted_m = data['M']
 
     # 9
-    connection.send_encrypted()
+    M = DH_decrypt(encrypted_m, KA, KB)
+    hash_m = sha1(M)
+    encrypted_hash_m = DH_encrypt(hash_m, KA, KB)
+    M_Prim = 'M_PRIM'
+    encrypted_m_prim = DH_encrypt(M_Prim, KA, KB)
+    connection.send_encrypted(
+        path='resume_session', data=dict(
+            to=other_user,
+            encrypted_hash_m=encrypted_hash_m,
+            encrypted_m_prim=encrypted_m_prim,
+            T=datetime.now().timestamp()
+        ),
+        headers=dict(
+            authentication=dict(
+                username=luser.username,
+                password=luser.password
+            )
+        ), public_key=settings.SERVER_PUB
+    )
 
 
 def message_to_user(other_user: str, message: str):
---
+++ chat/utils/defi_helman.py
@@ -0,0 +1,6 @@
+def DH_decrypt(message, KA, KB):
+    return message
+
+
+def DH_encrypt(message, KA, KB):
+    return message
